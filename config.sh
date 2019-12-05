d157181f031d06714ad99f599818e41a083fb7ad
tokane888 <tokane888@gmail.com>
2019-12-05 19:37:39 +0900

    設定をconfig.shに分離

---
+++ config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOT_FILES_DIR=".local/dotfiles/"
+APT_PACKAGES=(
+  curl
+  gcc
+  golang-go
+  make
+  nodejs
+  npm
+  openssh-server
+  tree
+  vim
+  wget
+)
+
+GO_GETS=(
+  github.com/golang/dep/cmd/dep
+)
--- install.sh
+++ install.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-
-DOT_FILES_DIR=".local/dotfiles/"
-APT_PACKAGES=(
-  curl
-  gcc
-  golang-go
-  make
-  nodejs
-  npm
-  openssh-server
-  tree
-  vim
-  wget
-)
-
-GO_GETS=(
-  github.com/golang/dep/cmd/dep
-)
+. config.sh
 
 set -eux
 
