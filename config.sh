333c21a223de8d867ac4c36a72c54dd73ba54fc2
tokane888 <tokane888@gmail.com>
2019-12-10 20:39:17 +0900

    公式リポジトリのnodejs, npmのバージョンが古すぎる(8, 3)ので削除

--- config.sh
+++ config.sh
@@ -10,8 +10,6 @@ APT_PACKAGES=(
   libclang-dev # vim補完するYouCompleteMeが依存
   locales # docker上で日本語使うため
   make
-  # nodejs
-  # npm
   openssh-server
   python3-dev # vim補完するYouCompleteMeが依存
   tree
