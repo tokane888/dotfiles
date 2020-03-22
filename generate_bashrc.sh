c8ec58d22426e999aa97d8f3d4708cabc53a9e50
tokane888 <tokane888@gmail.com>
2020-03-22 19:48:05 +0900

    別ファイル内のパス指定修正

--- generate_bashrc.sh
+++ generate_bashrc.sh
@@ -3,23 +3,25 @@
 set -eux
 
 generate_bashrc() {
-  cat .bashrc >>~/.bashrc
+  local home=$1
+
+  cat $home/.local/dotfiles/.bashrc >>$home/.bashrc
 
   # OS別設定
-  echo "# git入力補完" >>~/.bashrc
+  echo "# git入力補完" >>$home/.bashrc
   if [ -f /usr/share/bash-completion/completions/git ]; then
-    echo "source /usr/share/bash-completion/completions/git" >>~/.bashrc
+    echo "source /usr/share/bash-completion/completions/git" >>$home/.bashrc
   elif [ -f /etc/bash_completion.d/git ]; then
-    echo "source /etc/bash_completion.d/git" >>~/.bashrc
+    echo "source /etc/bash_completion.d/git" >>$home/.bashrc
   fi
   if [ -f /usr/lib/git-core/git-sh-prompt ]; then
-    echo "source /usr/lib/git-core/git-sh-prompt" >>~/.bashrc
+    echo "source /usr/lib/git-core/git-sh-prompt" >>$home/.bashrc
   elif [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
-    echo "source /usr/share/git-core/contrib/completion/git-prompt.sh" >>~/.bashrc
+    echo "source /usr/share/git-core/contrib/completion/git-prompt.sh" >>$home/.bashrc
   fi
 
   if [ -f /etc/bash_completion ]; then
-    echo "source /etc/bash_completion" >>~/.bashrc
+    echo "source /etc/bash_completion" >>$home/.bashrc
   else
     # CentOSではmakeの補完が機能しなかったため追加
     complete -W "\`grep -oE '^[a-zA-Z0-9_.-]+:([^=]|$)' ?akefile | sed 's/[^a-zA-Z0-9_.-]*$//'\`" make
