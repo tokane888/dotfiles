b5f3d5338d52b5c9cbc432af6608a95032bf0079
tokane888 <tokane888@gmail.com>
2020-03-22 18:33:56 +0900

    共通処理括り出し

--- install.sh
+++ install.sh
@@ -10,21 +10,16 @@ main() {
     sed -i -e 's/\(deb\|deb-src\) http:\/\/archive.ubuntu.com/\1 http:\/\/jp.archive.ubuntu.com/g' /etc/apt/sources.list
     apt-get update -y
     apt-get install -y git sudo
-    mkdir -p ~/.local
-    cd ~/.local
-    git clone https://github.com/tokane888/dotfiles.git
-    cd ~/.local/dotfiles
-    sudo ./core.sh
   elif [ $(command -v yum) ]; then
     yum remove -y git
     yum install -y https://centos7.iuscommunity.org/ius-release.rpm
     yum install -y git2u sudo
-    mkdir -p ~/.local
-    cd ~/.local
-    git clone https://github.com/tokane888/dotfiles.git
-    cd ~/.local/dotfiles
-    sudo ./core.sh
   fi
+  mkdir -p ~/.local
+  cd ~/.local
+  git clone https://github.com/tokane888/dotfiles.git
+  cd ~/.local/dotfiles
+  sudo ./core.sh
 }
 
 main
