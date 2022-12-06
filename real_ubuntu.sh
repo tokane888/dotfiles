fa9620943f3426022074df4740151a13a70c3a8e
tokane888 <tokane888@gmail.com>
2022-12-06 21:12:02 +0900

    feature: 実機ubuntuインストール対象にsnapd追加

---
+++ real_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DEB_PACKAGES=(
+  nmap
+  snapd
+)
+
+install_vscode() {
+  curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
+  apt-get install -y ./vscode.deb
+  mkdir -p /root/.config/Code
+}
+
+install_deb_packages() {
+  apt-get install -y ${DEB_PACKAGES[*]}
+}
+
+apt-get update -y
+install_deb_packages
