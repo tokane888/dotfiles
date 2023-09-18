dc2820e605ebcbf8e5486c2ad775a5ad9ba0b0d6
tokane888 <tokane888@gmail.com>
2023-09-18 14:16:42 +0900

    feature: tmuxのstatus barに実行中のtoggl項目表示する機能追加

---
+++ .tmux/right_status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# togglで進行中のタスクを表示
+function toggl_now() {
+  #local tgn=$(toggl now)
+  local tgn_time=$(toggl now | grep Duration | cut -d ' ' -f 2)
+  local tgn_dsc=$(toggl now | head -1 | grep -oP '.*(?= #.*)')
+  local short_tgn_dsc=$(if [ $(echo $tgn_dsc | wc -m) -lt 20 ]; then echo $tgn_dsc; else echo "${tgn_dsc}.."; fi)
+  if [ ! -n "$tgn_time" ]; then
+      echo ""
+  else
+      echo "[$tgn_time $short_tgn_dsc]"
+  fi
+}
+
+toggl_now
