f3754bfdab6f422ed68972613ec41933abde2c14
tokane888 <tokane888@gmail.com>
2024-01-27 16:42:18 +0900

    fix: 変換+lが機能していなかったので修正

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 # 日本語入力設定
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
 # super + lによるlock無効化
-gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]"
 # ctrl + . => 下線付きの_ 無効化
 gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"
 # win + d => desktop表示 無効化
