f46c380123b72348836aa324d04cd6ae165bec65
tokane888 <tokane888@gmail.com>
2024-01-27 14:17:31 +0900

    fix: 重複処理削除

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -2,21 +2,3 @@
 
 set -euxo pipefail
 
-DEB_PACKAGES=(
-  nmap
-  snapd
-  xclip
-)
-
-install_vscode() {
-  curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
-  apt-get install -y ./vscode.deb
-  mkdir -p /root/.config/Code
-}
-
-install_deb_packages() {
-  apt-get install -y ${DEB_PACKAGES[*]}
-}
-
-apt-get update -y
-install_deb_packages
