d7dae63ba79027a3ce92b0b8ea16890b5b609fd5
tokane888 <tokane888@gmail.com>
2024-02-04 01:25:37 +0900

    refactor: 余計なコメント削除

--- .tmux/right_status.sh
+++ .tmux/right_status.sh
@@ -2,7 +2,6 @@
 
 # togglで進行中のタスクを表示
 function toggl_now() {
-  #local tgn=$(toggl now)
   local tgn_time=$(toggl now | grep Duration | cut -d ' ' -f 2)
   local tgn_dsc=$(toggl now | head -1 | grep -oP '.*(?= #.*)')
   local short_tgn_dsc=$(if [ $(echo $tgn_dsc | wc -m) -lt 20 ]; then echo $tgn_dsc; else echo "${tgn_dsc}.."; fi)
