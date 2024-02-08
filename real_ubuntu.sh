4da1bac1bdb9e2b9d2233bc354a861f5f664b053
tokane888 <tokane888@gmail.com>
2024-02-09 07:52:32 +0900

    feature: ubuntuでの画面4分割対応のため、winTile追加

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 
 # gnome shell extensionのTime++導入(確認ダイアログが表示される)
 gnome-extensions-cli install timepp@zagortenay333
+# winTileインストール。画面4分割可能に
+gnome-extensions-cli install wintile@nowsci.com
 
 # 日本語入力設定
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
