b2b7f8b64c0e69920a34ad0bd82c9051854c77f2
tokane888 <tokane888@gmail.com>
2024-02-10 15:32:14 +0900

    refactor: nerd-font install済みの場合は関連処理省略するよう改修

--- core.sh
+++ core.sh
@@ -338,7 +338,7 @@ install_main_snap_packages() {
 }
 
 install_nerd_font() {
-  if [ -f "${HOME%/}/ghq/github.com/ryanoasis/nerd-fonts" ]; then
+  if [ -d "${HOME%/}/ghq/github.com/ryanoasis/nerd-fonts" ]; then
     return
   fi
 
