576dafbeb610ff415e08fdd49783ae2d36feff07
tokane888 <tokane888@gmail.com>
2024-02-17 15:13:26 +0900

    docs: vscode install前にapt-transport-httpsをインストールする理由をコメントで追記

--- core.sh
+++ core.sh
@@ -308,6 +308,7 @@ prepare_vscode_install() {
   install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
   sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
   rm -f packages.microsoft.gpg
+  # vscode install処理時にapt repositoryへのアクセスにhttpsが使用されるため、apt-transport-httpsの事前installが必要
   apt-get install -y apt-transport-https
 }
 
