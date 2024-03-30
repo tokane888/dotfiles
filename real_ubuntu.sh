745319a4cc6a17380fa42ba9791a8a8215dbdfe2
tokane888 <tokane888@gmail.com>
2024-03-30 18:50:09 +0900

    super + lによるlock無効化を、super + sによるlockに置き換えす想定でalt+sに置き換えていたので修正

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -10,7 +10,7 @@ gnome-extensions-cli install wintile@nowsci.com
 # 日本語入力設定
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
 # lockのショートカットをsuper + l => super + sに置き換え
-gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "['<Alt>s']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "['<Super>s']"
 # ctrl + . => 下線付きの_ 無効化
 gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"
 # win + d => desktop表示 無効化
