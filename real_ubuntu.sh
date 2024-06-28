c1eb67b76df46f5e7cb0020fcaadce5f9bca993c
tokane888 <tokane888@gmail.com>
2024-06-29 07:33:31 +0900

    fix: lockのショートカットが機能していなかったので一旦無効化

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -9,8 +9,6 @@ gnome-extensions-cli install wintile@nowsci.com
 
 # 日本語入力設定
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
-# lockのショートカットをsuper + l => super + 1に置き換え
-gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "['<Super>1']"
 # ctrl + . => 下線付きの_ 無効化
 gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"
 # win + d => desktop表示 無効化
