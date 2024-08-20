c7f07c88d325d35e6a5ad3afd92986e6d8591360
tokane888 <tokane888@gmail.com>
2024-08-21 06:47:24 +0900

    refactor: activity watch 最新化

--- core.sh
+++ core.sh
@@ -280,8 +280,8 @@ setup_ubuntu() {
 
 install_real_deb_packages() {
   # localhost:5600でdashboardが見られるが、OS再起動するまでは見えない
-  curl -LO https://github.com/ActivityWatch/activitywatch/releases/download/v0.12.2/activitywatch-v0.12.2-linux-x86_64.deb
-  dpkg -i activitywatch-v0.12.2-linux-x86_64.deb
+  curl -LO https://github.com/ActivityWatch/activitywatch/releases/download/v0.13.1/activitywatch-v0.13.1-linux-x86_64.deb
+  dpkg -i activitywatch-v0.13.1-linux-x86_64.deb
 
   for repo in "${MAIN_PC_APT_REPOS[@]}"; do
     add-apt-repository -y "$repo"
