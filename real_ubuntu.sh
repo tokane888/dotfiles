147c22ec9f7daa3d2c2741c769e50b3b6a0600a7
tokane888 <tokane888@gmail.com>
2025-11-03 21:08:39 +0900

    refactor: removed unused gnome extensions

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -2,11 +2,6 @@
 
 set -euxo pipefail
 
-# gnome shell extensionのTime++導入(確認ダイアログが表示される)
-gnome-extensions-cli install timepp@zagortenay333
-# winTileインストール。画面4分割可能に
-gnome-extensions-cli install wintile@nowsci.com
-
 # 日本語入力設定
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
 # ctrl + . => 下線付きの_ 無効化
