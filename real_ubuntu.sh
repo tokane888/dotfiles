16f75a553e2a09fdc36665a6b2dd225711b54046
tokane888 <tokane888@gmail.com>
2025-12-13 10:41:54 +0900

    refactor: ubuntu実機向けscriptのgsettings関連設定を関数にくくり出し

--- real_ubuntu.sh
+++ real_ubuntu.sh
@@ -2,11 +2,19 @@
 
 set -euxo pipefail
 
-# 日本語入力設定
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
-# ctrl + . => 下線付きの_ 無効化
-gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"
-# win + d => desktop表示 無効化
-gsettings set org.gnome.desktop.wm.keybindings show-desktop "[]"
-# 変換 + f => window最大化(変換+jは何かと競合したのか設定出来ず)
-gsettings set org.gnome.desktop.wm.keybindings maximize "['<Hyper>f']"
+setup_gsettings() {
+    # 日本語入力設定
+    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
+    # ctrl + . => 下線付きの_ 無効化
+    gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"
+    # win + d => desktop表示 無効化
+    gsettings set org.gnome.desktop.wm.keybindings show-desktop "[]"
+    # 変換 + f => window最大化(変換+jは何かと競合したのか設定出来ず)
+    gsettings set org.gnome.desktop.wm.keybindings maximize "['<Hyper>f']"
+}
+
+main() {
+    setup_gsettings
+}
+
+main
